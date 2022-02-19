38c28debfe2a3618dcaa8e0d38e24471ffbf46e7
RutaTang <953259325@qq.com>
2022-02-19 13:59:49 +0800

    Add tabular and vim-markdown
    add these two plugins for text processing and markdown editing

--- Navchad/custom/plugins/init.lua
+++ Navchad/custom/plugins/init.lua
@@ -1,6 +1,14 @@
 return {
+  -- lsp server installer
   {"williamboman/nvim-lsp-installer"},
+  -- surrond editing
   {"tpope/vim-surround"},
+  -- AI Coding
   {"github/copilot.vim"},
+  -- Markdown previewer in neovim
   {"ellisonleao/glow.nvim"},
+  -- text aligner
+  {"godlygeek/tabular"},
+  -- markdown syntax highlighting and folding
+  {"preservim/vim-markdown"},
 }
