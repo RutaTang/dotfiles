61140e5d62c8bfd03707e01a9d7b9b4ca4287b78
RutaTang <953259325@qq.com>
2022-02-20 01:59:30 +0800

    add more bufferline keymappings

--- Navchad/custom/mappings.lua
+++ Navchad/custom/mappings.lua
@@ -8,6 +8,15 @@ map("n", "<leader>q", ":q <CR>")
 vim.api.nvim_set_keymap('i','<C-j>','copilot#Accept("<CR>")',{expr=true,silent=true})
 vim.g.copilot_no_tab_map = true
 
--- Bufferline Mapping
-vim.api.nvim_set_keymap('n','<leader>o',':BufferLineCloseLeft<CR> :BufferLineCloseRight<CR>',{noremap=true, silent=true})
+-- ===Bufferline Mapping===
+-- Close others
+vim.api.nvim_set_keymap('n','<leader>bo',':BufferLineCloseLeft<CR> :BufferLineCloseRight<CR>',{noremap=true, silent=true})
+-- Pick buffer
+vim.api.nvim_set_keymap('n','<leader>bp',':BufferLinePick<CR>',{noremap=true, silent=true})
+-- Pick buffer Close
+vim.api.nvim_set_keymap('n','<leader>bc',':BufferLinePickClose<CR>',{noremap=true, silent=true})
+-- Move bufer to prev 
+vim.api.nvim_set_keymap('n','<leader>bh',':BufferLineMovePrev<CR>',{noremap=true, silent=true})
+-- Move bufer to next 
+vim.api.nvim_set_keymap('n','<leader>bl',':BufferLineMoveNext<CR>',{noremap=true, silent=true})
 
